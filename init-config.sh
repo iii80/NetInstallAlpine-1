4ad0b1acd5de9d80a253fd88bc86787ac0431188
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2022-12-24 10:52:35 +0800

    Update init-config.sh

--- init-config.sh
+++ init-config.sh
@@ -17,18 +17,11 @@ EOF
 sysctl -p
 
 # 软件安装
-apk update && apk add procps bash bash-completion vim curl wget net-tools docker docker-compose vnstat
-sed -i "s#/bin/ash#/bin/bash#g" /etc/passwd
+apk update && apk add procps bash bash-completion vim curl wget net-tools docker docker-compose vnstat zram-init
 
-# 服务自动
-rc-service docker start
-rc-service vnstatd start
-
-# 添加开机自启
-rc-update add docker
-rc-update add vnstatd
 
 # 配置
+sed -i "s#/bin/ash#/bin/bash#g" /etc/passwd
 cat >~/.bash_profile<<EOF
 alias update='apk update && apk upgrade'
 export HISTTIMEFORMAT="%d/%m/%y %T "
@@ -39,6 +32,38 @@ alias ll='ls -alF'
 alias ls='ls --color=auto'
 source /etc/profile.d/bash_completion.sh
 EOF
+cat>/etc/conf.d/zram-init<<EOF
+load_on_start=yes
+unload_on_stop=yes
+
+num_devices=1
+
+type0=swap
+flag0=
+size0=`LC_ALL=C free -m | awk '/^Mem:/{print int($2/1)}'`
+mlim0=`LC_ALL=C cat /proc/cpuinfo | grep "processor" | wc -l`
+back0=
+icmp0=
+idle0=
+wlim0=
+notr0=
+maxs0=1
+algo0=zstd
+labl0=zram_swap
+uuid0=
+args0=
+
+EOF
+
+# 服务自动
+rc-service docker start
+rc-service vnstatd start
+
+# 添加开机自启
+rc-update add docker
+rc-update add vnstatd
+
+
 
 
 
