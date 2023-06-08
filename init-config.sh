8742a7d4030afa341a3b3b997f46094019ccf7ba
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2023-06-08 22:54:34 +0800

    Update init-config.sh

--- init-config.sh
+++ init-config.sh
@@ -20,7 +20,8 @@ sysctl -p
 
 
 # 软件安装
-apk update && apk add coreutils iproute2 tzdata procps bash bash-completion vim curl wget net-tools docker docker-compose vnstat zram-init 
+apk update && apk add coreutils iproute2 tzdata procps bash bash-completion vim curl wget net-tools docker vnstat zram-init
+
 
 # 配置
 
